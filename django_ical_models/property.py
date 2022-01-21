3bf70c4eacd1df191c1f4f39848a147baf5c4764
Robert Spralja <robert.spralja@gmail.com>
2022-01-21 22:58:54 +0100

    changed NotImplementedError

--- django_ical_models/property.py
+++ django_ical_models/property.py
@@ -1,51 +1,32 @@
 from django.db import models
 
 
-match_value_data_type_to_model = {
-    'BINARY': None,
+value_data_type_to_model = {
+    'BINARY': NotImplementedError,
     'BOOLEAN': models.BooleanField,
-    'CAL-ADDRESS': None,
+    'CAL-ADDRESS': NotImplementedError,
     'DATE': models.DateField,
     'DATE-TIME': models.DateTimeField,
-    'DURATION': None,
+    'DURATION': NotImplementedError,
     'FLOAT': models.FloatField,
     'INTEGER': models.IntegerField,
-    'PERIOD': None,
-    'RECUR': None,
+    'PERIOD': NotImplementedError,
+    'RECUR': NotImplementedError,
     'TEXT': models.TextField,
     'TIME': models.TimeField,
-    'URI': None,
-    'UTC-OFFSET': None, 
+    'URI': NotImplementedError,
+    'UTC-OFFSET': NotImplementedError, 
 }
-
-
-def match_value_data_type_to_model(value_data_type):
-    match value_data_type:
-        case 'BOOLEAN':
-            return models.BooleanField
-        case 'DATE':
-            return models.DateField
-        case 'DATE-TIME':
-            return models.DateTimeField
-        case 'FLOAT':
-            return models.FloatField
-        case 'INTEGER':
-            return models.IntegerField
-        case 'TEXT':
-            return models.TextField
-        case 'TIME':
-            return models.TimeField
-        case 'BINARY' | 'CAL-ADDRESS' | 'DURATION' | 'PERIOD' | 'RECUR' | 'URI' | 'UTC-OFFSET':
-            raise NotImplementedError('value_data_type \'%s\' not implemented' % value_data_type)
-        case _:
-            raise TypeError('value_data_type \'%s\' not supported' % value_data_type)
 
 
 def create_property_field(value_data_type, conformance, **options):
     if not isinstance(value_data_type, str):
         raise TypeError(f'value_data_type must be str, not {type(value_data_type).__qualname__}')
 
-    field = match_value_data_type_to_model(value_data_type)
+    if value_data_type not in value_data_type_to_model:
+        raise TypeError(f'value_data_type \'{value_data_type}\' not supported')
+    
+    field = value_data_type_to_model[value_data_type]
 
     if not isinstance(conformance, str):
         raise TypeError(f'conformance must be str, not {type(conformance).__qualname__}')
